72fad88c9f94caa92ed4c37f4b461ad62b7eb684
LemmoLem <liampooleo@gmail.com>
2023-05-09 17:23:13 +0100

    mo edits, main screen n build

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -52,7 +52,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 
     public void SetHandSize(int amount)
